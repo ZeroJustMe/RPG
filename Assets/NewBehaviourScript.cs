0886575d1acd20659332eea2add3f2f3912eff31
ROROBINBIN <464214931@qq.com>
2017-12-25 22:04:38 +0800

    demo

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(h * Time.deltaTime * (float)3.8, v * Time.deltaTime * (float)3.0, 0);
+        transform.position += move;
+    }
+}
